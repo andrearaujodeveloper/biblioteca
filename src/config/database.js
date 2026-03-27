25cccecf1453ab79fba6b0c826eca1e9759e424b
andre <andrearaujodeveloper@gmail.com>
2026-03-27 15:26:40 -0300

    config dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 const {Pool}= require('pg');
 
 const pool = new Pool({
-    host:'localhost',
-    port:5432,
-    user:'api_user',
-    password:'api_password',
-    database:'minha_api'
+    host: process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE
 })
 
 module.exports = pool;
