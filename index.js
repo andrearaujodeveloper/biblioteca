95e9747bbc567e6ab5be24fe094f8096b82a3062
andre <andrearaujodeveloper@gmail.com>
2026-03-29 20:42:14 -0300

    metodo para testes servidor

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api',routes);
 app.get('/',(req, res)=>{
-    res.status(200).json({status: ativo})
+    res.send('API Online!');
 });
 
 app.use(error);
