6138e98026db98db492cff24e283380aefb59413
andre <andrearaujodeveloper@gmail.com>
2026-03-29 21:04:26 -0300

    ajuste conexãodb

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,12 @@
 const {Pool}= require('pg');
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
+    /*host: process.env.DB_HOST,
     port:process.env.DB_PORT,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE,
+    database:process.env.DB_DATABASE,*/
+    connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
 })
 
